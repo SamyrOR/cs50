d957f551c8fbacd12abb5b42f18032c46b7cc5ae
Samyr Ribeiro <saamyr@live.com>
2025-01-09 00:17:35 -0300

    feat: updated cat program to be more dynamic

--- week-1/cat.c
+++ week-1/cat.c
@@ -1,11 +1,26 @@
+#include <cs50.h>
 #include <stdio.h>
 
-void meow(int n);
+// In C is important to declare the function signature
+// Since its doesnt have hoisting
+int get_positive_int(void);
+void meow(int times);
 
-int main(void) { meow(3); }
+int main(void) {
+  int times = get_positive_int();
+  meow(times);
+}
 
-void meow(int n) {
-  for (int i = 0; i < 3; i++) {
+int get_positive_int(void) {
+  int n;
+  do {
+    n = get_int("How many times to meow?\n");
+  } while (n < 1);
+  return n;
+}
+
+void meow(int times) {
+  for (int i = 0; i < times; i++) {
     printf("meow\n");
   }
 }
